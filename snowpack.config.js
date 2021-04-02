958403d1608d0abc5cb5958669f9a7a261d180c2
Etesam Ansari <etesamansari13@gmail.com>
2021-04-02 00:56:40 -0400

    :tada: Configured Snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,12 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+      // directory name: 'build directory'
+      public: '/',
+      src: '/dist',
   },
   plugins: [
-    /* ... */
+      '@snowpack/plugin-react-refresh'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
---
+++ src/styling/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+    font: {
+        mainFont: "",
+        secondaryFont: ""
+    },
+    colors:{
+
+    }
+}
+
+export const darkTheme = {
+    font: {
+        mainFont: "",
+        secondaryFont: ""
+    },
+    colors:{
+
+    }
+}
